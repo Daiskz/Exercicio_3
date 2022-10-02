f36f5eb17621d297888a464e27e169e497aa4cf5
Daiskz <gabriel.matsubara@gmail.com>
2022-10-02 12:58:26 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int numero2 = sc.nextInt();
+        int soma = numero1 + numero2;
+        System.out.println();
+        System.out.println("A soma é: " + soma);
+    }
+}
